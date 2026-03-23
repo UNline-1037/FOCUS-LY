0ce12ab34e85b34a7e87eeda129cff3a1e7bc159
UNline <3436637986@qq.com>
2026-03-23 23:58:17 +0800

    Add files via upload

--- FOCUS/test1.py
+++ FOCUS/test1.py
@@ -1,12 +1,16 @@
-from flask import Flask,request,jsonify
+from flask import Flask, request, jsonify, render_template
 from flask_cors import CORS
 import markdown
 
 app = Flask(__name__)
 CORS(app)
 
+@app.route('/')
+def test():
+    return render_template("test1.html")
 
-@app.route('/',methods=['GET','POST'])
+
+@app.route('/post', methods=['POST','GET'])
 def index():
     data=request.get_json()
     md_content =data['md_text']
@@ -27,4 +31,4 @@ def index():
     )
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
